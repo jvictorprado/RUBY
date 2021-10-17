46b990b60a61b4e61e854c1cfe61a5f5c866d6e4
Victor Prado <victorpradoestudo@gmail.com>
2021-10-17 20:19:50 -0300

    s1a1

---
+++ Seção 1/s1a1.rb
@@ -0,0 +1,16 @@
+# EM KOTLIN NÃO É PRECISO DEFINIR UM TIPO PARA AS VARIÁVEIS
+numero = 10
+
+# TAMBÉM NÃO É PRECISO O USO DE PARENTESES AO DECLARAR UM MÉTODO
+def soma a,b
+    a+b
+end
+
+# A ÚLTIMA LINHA DO MÉTODO É SEMPRE O RETURN 
+def muda_num
+    numero = 145
+    puts numero
+end
+
+muda_num
+puts numero
